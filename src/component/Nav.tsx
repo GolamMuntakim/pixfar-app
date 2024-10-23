c35f5c427d00a2d3b5c527df17855d4dd1f380ec
Golam Muntakim <seams123456789@gmail.com>
2024-10-22 21:18:41 -0700

    nav change

--- src/component/Nav.tsx
+++ src/component/Nav.tsx
@@ -6,7 +6,7 @@ const Nav = () => {
         <div className="w-full bg-cyan-950 text-white  fixed mt-0 z-50">
             <div className="flex justify-between px-10 py-4">
                 <div>
-                    <Link to="/" onFocus={window.location.reload}><h1 className="font-serif text-2xl">Pixfar Shop</h1></Link>
+                    <Link to="/" onFocus={window.location.reload}><h1 className="font-serif text-2xl text-yellow-600">Pixfar Shop</h1></Link>
                 </div>
                 <div>
                     <ul className="flex gap-4 font-serif text-2xl">
